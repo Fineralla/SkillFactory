9e3787dfb624d4434c81e2299fe27ad78df589fd
Irina <50765675+Fineralla@users.noreply.github.com>
2023-10-10 12:13:17 +0300

    Add files via upload

---
+++ module 18/main.cpp
@@ -0,0 +1,11 @@
+#include "Chat/ChatRun.h"
+typedef unsigned int uint;
+
+// команда для очистки консоли
+const char clear[]{"clear"};
+
+int main(int argc, const char *argv[])
+{
+    ChatRun chat;
+    chat.Run();
+}
